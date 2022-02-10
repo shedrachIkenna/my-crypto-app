c1da0dd6fb80b4d107441acaad32f4cdc4cdc398
shedrachIkenna <shedrach686@gmail.com>
2022-02-10 13:11:20 +0100

    Basic app setup with necessary imports included

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons'
+import logo from '../images/logo.png'
+
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={logo} size="large"/>
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">CryptoSpace</Link>
+                </Typography.Title>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
+
